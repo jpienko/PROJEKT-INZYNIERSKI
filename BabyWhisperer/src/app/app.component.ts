b94c2cb1e8ab81523841f03f772086679453254e
jpienko <33654474+jpienko@users.noreply.github.com>
2018-08-27 13:56:39 +0200

    menuToggle
    only daybook page added

--- BabyWhisperer/src/app/app.component.ts
+++ BabyWhisperer/src/app/app.component.ts
@@ -1,22 +1,35 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, MenuController, Nav} from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import { DaybookPage }from '../pages/daybooks/daybook/daybook'
+import { SchedulesPage }from '../pages/schedule/schedules/schedules'
 import { HomePage } from '../pages/home/home';
+
 @Component({
   templateUrl: 'app.html'
 })
-export class MyApp {
-  rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+export class MyApp {
+  @ViewChild(Nav) nav:Nav;
+  rootPage:any = HomePage;
+  pages: Array<{ title: string, component: any }>;
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private menu: MenuController) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
+
       statusBar.styleDefault();
       splashScreen.hide();
+      
     });
+    this.pages=[
+        {title: "Dzienniki", component: DaybookPage},
+        {title: "Harmonogramy", component:SchedulesPage }
+      ];
+  }
+  public openPage(page){
+    var name:string = page
+     this.nav.push(name)
   }
 }
 
